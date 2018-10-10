b95b66c61f3c01249a2750ab6a9fb6e3ad9efcfb
macbeth322 <chrisp533@gmail.com>
2018-10-09 21:21:01 -0400

    repro

---
+++ from_imp.py
@@ -0,0 +1,3 @@
+from typedpkg_namespace.alpha.alpha_module import alpha_func
+alpha_func(False)
+alpha_func(2)
---
+++ typedpkg_namespace-alpha/not_from_imp.py
@@ -0,0 +1,3 @@
+import typedpkg_namespace.alpha.alpha_module as am; alpha_func = am.alpha_func
+alpha_func(False)
+alpha_func(2)
